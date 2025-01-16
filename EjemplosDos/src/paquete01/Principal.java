ed64cc0dc5ad3ece9c1fee72194ff99cc476be7e
Javier Vinan <javinan5@utpl.edu.ec>
2025-01-16 12:25:17 -0500

    cambios

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -6,6 +6,7 @@
 package paquete01;
 
 import paquete02.DatoPersonal;
+import paquete02.DatoTrabajo;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
@@ -18,8 +19,11 @@ public class Principal {
 
     public static void main(String[] args) {
         String nombre;
+        String nombreEmpresa;
+        String direccionEmpresa;
         String ciudad;
         String apellidoRetornado;
+        double mejorNota;
         
         double[] misNotas;
         double promedio;
@@ -27,19 +31,27 @@ public class Principal {
         nombre = DatoPersonal.obtenerNombre();
         apellidoRetornado = DatoPersonal.obtenerApellido();
         ciudad = DatosUbicacion.obtenerCiudad();
+        nombreEmpresa = DatoTrabajo.obtenerNombreEmpresa();
+        direccionEmpresa = DatoTrabajo.obtenerDireccionEmpresa();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
+        mejorNota = DatoFinal.obtenerMejorNota(misNotas);
         
         
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
                 + "Ciudad: %s\n"
-                + "Promedio: %.2f\n",
+                + "Nombre Empresa: %s - Direccion Empresa: %s\n"
+                + "Promedio: %.2f\n"
+                + "Mejor Nota: %.2f\n",
                 nombre,
                 apellidoRetornado,
                 ciudad,
-                promedio);
+                nombreEmpresa,
+                direccionEmpresa,
+                promedio,
+                mejorNota);
     }
     
 }
--- EjemplosDos/src/paquete05/DatoFinal.java
+++ EjemplosDos/src/paquete05/DatoFinal.java
@@ -23,4 +23,31 @@ public class DatoFinal {
         promedio = suma / n.length;
         return promedio;
     }
+    public static double obtenerMejorNota(double[] a){
+        double mayor = a[0];
+        for(int i = 0; i < a.length; i++){
+            if(mayor < a[i]){
+                mayor = a[i];
+            }
+            
+        }
+        /*
+        if(n[0] > n[1] && n[0]> n[2] && n[0] > n[3]){
+            mayor = n[0];
+        }else{
+            if(n[1] > n[0] && n[1]> n[2] && n[1] > n[3]){
+                mayor = n[1];
+            }else{
+                if(n[2] > n[0] && n[2]> n[1] && n[2] > n[3]){
+                    mayor = n[2];
+                }else{
+                    if(n[3] > n[0] && n[3]> n[1] && n[3] > n[2]){
+                        mayor = n[3];
+                    }
+                }
+            }
+        }
+        */
+        return mayor;
+    }
 }
